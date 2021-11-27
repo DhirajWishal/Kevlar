f52f265ec5cf61023a5dbdf303135ad88fe344f1
DhirajWishal <wishaldhiraj@gmail.com>
2021-11-27 16:19:05 +0530

    Updated the build script so that we wont unnecessarily build the premake project if the package already exists.

--- Scripts/Build.py
+++ Scripts/Build.py
@@ -2,8 +2,18 @@
 
 import os
 
-# Build premake5 used to build the repository.
-os.system("cd \"ThirdParty/premake5\" && make -f Bootstrap.mak linux")
+"""
+Build and run using the included third party premake library source.
+"""
+def build_and_run_premake():
+	# Build premake5 used to build the repository.
+	os.system("cd \"ThirdParty/premake5\" && make -f Bootstrap.mak linux")
 
-# Build the makefiles for the projects.
-os.system("\"ThirdParty/premake5/bin/release/premake5\" gmake2")
+	# Build the makefiles for the projects.
+	os.system("\"ThirdParty/premake5/bin/release/premake5\" gmake2")
+
+if os.system("command -v premake5") > 0:
+	build_and_run_premake()
+
+else:
+	os.system("premake5 gmake2")
