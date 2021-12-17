c4b779854a12d07017861a8d23854cd4f20a55c9
Dhiraj Wishal <wishaldhiraj@gmail.com>
2021-12-18 00:09:15 +0530

    Minor edits to check if the client and the server can send data without an issue

--- client/src/com/kevlar/Main.java
+++ client/src/com/kevlar/Main.java
@@ -1,8 +1,16 @@
 package com.kevlar;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
+			byte[] bytes = Hasher.getSHA256("Thidas").getBytes(StandardCharsets.UTF_8);
+			System.out.println(new String(bytes, StandardCharsets.UTF_8));
+			Sender sender = new Sender(Base64.getEncoder().encodeToString(bytes));
+			System.out.println(sender.getResponse());
+
 			Application application = new Application();
 			application.run();
 			application.cleanup();
