0fc4b3915ce970fd5b1270394526447d0a526359
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-01-04 18:07:33 +0530

    Minor formatting improvements.

--- client/src/com/kevlar/Application.java
+++ client/src/com/kevlar/Application.java
@@ -1,7 +1,5 @@
 package com.kevlar;
 
-import com.kevlar.client.*;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -9,21 +7,18 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Scanner;
 
 public abstract class Application {
     /**
      * Run the application.
      *
      * @throws InvalidAlgorithmParameterException This function can throw an invalid algorithm parameter exception.
-     * @throws NoSuchPaddingException This function can throw a no such padding exception.
-     * @throws IllegalBlockSizeException This function can throw an illegal block size exception.
-     * @throws NoSuchAlgorithmException This function can throw a no such algorithm exception.
-     * @throws BadPaddingException This function can throw a bad padding exception.
-     * @throws InvalidKeyException This function can throw an invalid key exception.
-     * @throws IOException this function can throw an IO exception.
+     * @throws NoSuchPaddingException             This function can throw a no such padding exception.
+     * @throws IllegalBlockSizeException          This function can throw an illegal block size exception.
+     * @throws NoSuchAlgorithmException           This function can throw a no such algorithm exception.
+     * @throws BadPaddingException                This function can throw a bad padding exception.
+     * @throws InvalidKeyException                This function can throw an invalid key exception.
+     * @throws IOException                        this function can throw an IO exception.
      */
     public abstract void run() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, IOException;
 
